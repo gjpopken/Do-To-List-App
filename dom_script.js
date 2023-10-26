6a77351718a38c09fca6c787b43ce99b82d19ddf
Gavin Popken <gavinpopkenart@gmail.com>
2023-10-26 15:34:14 -0500

    created functionality for tasks, now app is technically done

--- dom_script.js
+++ dom_script.js
@@ -1,8 +1,10 @@
 //let itemList = []; //instialize as an array with brackets, thanks ChatGPT
 //much of this code was derived from a video tutorial found here : https://www.youtube.com/watch?v=G0jO8kUrg-I
+let list = document.getElementById("to-do-list");
+
 function addNewItem(){
     let newItem = document.getElementById("new-item").value;
-    let list = document.getElementById("to-do-list");
+
 if (newItem !== "") {
 console.log("There's something here.");
 //itemList.push(newItem);
@@ -22,3 +24,13 @@ document.getElementById("new-item").value = "";
 
 let addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addNewItem);
+
+//This script below is from the video tutorial
+list.addEventListener("click", function(e) { // the var e is arbitrary?
+    if (e.target.tagName === "LI"){ //not sure why this had to be capitalized
+        e.target.classList.toggle("checked");
+    } 
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false); //not sure why it says false at the end. 
