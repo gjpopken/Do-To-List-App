60624591b04743ef9a22038a919da95324c9e2ca
Gavin Popken <gavinpopkenart@gmail.com>
2023-10-26 13:45:51 -0500

    Trying to fix the code so it adds an item

--- dom_script.js
+++ dom_script.js
@@ -1,13 +1,20 @@
-let itemList = [];
+let itemList = []; //instialize as an array with brackets, thanks ChatGPT
 let addButton = document.getElementById("add-button");
 
 function addNewItem(){
     let newItem = document.getElementById("new-item").value;
+    let list = document.getElementsByTagName("ul");
 if (newItem !== "") {
 console.log("There's something here.");
-itemList.unshift(newItem);
-document.getElementById("new-item").value = "";
+itemList.push(newItem);
+
 console.log(itemList);
+//code below from mdn web docs
+const newShownItem = document.createElement("li");
+newShownItem.innerHTML = newItem;
+//list.appendChild(newShownItem);
+
+document.getElementById("new-item").value = "";
 } else {
 alert("Please add an item to add.");
 }
