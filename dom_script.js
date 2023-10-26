a99b1f1cf8e8dd0dda6efdbe094f371da0374a13
Gavin Popken <gavinpopkenart@gmail.com>
2023-10-26 14:32:12 -0500

    fixed code in JS and HTML so that items now properly add

--- dom_script.js
+++ dom_script.js
@@ -1,22 +1,20 @@
-let itemList = []; //instialize as an array with brackets, thanks ChatGPT
-let addButton = document.getElementById("add-button");
-
+//let itemList = []; //instialize as an array with brackets, thanks ChatGPT
 function addNewItem(){
     let newItem = document.getElementById("new-item").value;
-    let list = document.getElementsByTagName("ul");
+    let list = document.getElementById("to-do-list");
 if (newItem !== "") {
 console.log("There's something here.");
-itemList.push(newItem);
-
-console.log(itemList);
+//itemList.push(newItem);
+//console.log(itemList);
 //code below from mdn web docs
-const newShownItem = document.createElement("li");
+let newShownItem = document.createElement("li");
 newShownItem.innerHTML = newItem;
-//list.appendChild(newShownItem);
-
-document.getElementById("new-item").value = "";
+list.appendChild(newShownItem);
 } else {
 alert("Please add an item to add.");
 }
+document.getElementById("new-item").value = "";
 }
+
+let addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addNewItem);
